feb8c6804ac09d202d73d1f8f5ffd306bd73b3c3
Harshit <harshit10541@gmail.com>
2024-11-04 13:05:37 +0530

    password hashing and user login functionality done

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const errorHandler = require("./middleware/errorhandler");
 const dotenv = require("dotenv").config();
 const connectDb = require("./config/dbConnection");
 
-connectDb();
-const app = express();
+connectDb(); //connecting database
+const app = express();  //middleware
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts",require("./routes/contactRoutes"));
+app.use("/api/users",require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
